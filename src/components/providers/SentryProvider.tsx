a3557193c94f7394da7e1a04d4e886c441917b6f
jewelshot <jewelshot.app@gmail.com>
2025-11-27 23:47:01 +0300

    fix: update Sentry DSN to correct project ID

--- src/components/providers/SentryProvider.tsx
+++ src/components/providers/SentryProvider.tsx
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 export function SentryProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     Sentry.init({
-      dsn: "https://136322cdc9532b260f074a5f6a7b4d7a@o4510311307149312.ingest.de.sentry.io/4510437651775568",
+      dsn: "https://83cb51d732742f39d12b94b88c229ecf@o4510311307149312.ingest.de.sentry.io/4510438847414352",
       tracesSampleRate: 1.0,
       replaysSessionSampleRate: 0.1,
       replaysOnErrorSampleRate: 1.0,
